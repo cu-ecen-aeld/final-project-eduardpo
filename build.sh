131a875473847d8107e908d4d5c524645af649e6
Eduard Polyakov <eduardpo@gmail.com>
2025-04-10 13:50:57 +0300

    rearange bb layers

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git submodule update
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env
 
-CONFLINE="MACHINE = \"qemuarm64\""
+CONFLINE="MACHINE = \"raspberrypi4-64\""
 
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
@@ -31,6 +31,86 @@ if [ $layer_info -ne 0 ];then
 	bitbake-layers add-layer ../meta-aesd
 else
 	echo "meta-aesd layer already exists"
+fi
+
+bitbake-layers show-layers | grep "poky/meta" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding poky/meta layer"
+	bitbake-layers add-layer ../poky/meta
+else
+	echo "poky/meta layer already exists"
+fi
+
+bitbake-layers show-layers | grep "poky/meta-poky" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding poky/meta-poky layer"
+	bitbake-layers add-layer ../poky/meta-poky
+else
+	echo "poky/meta-poky layer already exists"
+fi
+
+bitbake-layers show-layers | grep "poky/meta-yocto-bsp" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding poky/meta-yocto-bsp layer"
+	bitbake-layers add-layer ../poky/meta-yocto-bsp
+else
+	echo "poky/meta-yocto-bsp layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-openembedded/meta-oe" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-openembedded/meta-oe layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-oe
+else
+	echo "meta-openembedded/meta-oe layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-openembedded/meta-python" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-openembedded/meta-python layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-python
+else
+	echo "meta-openembedded/meta-python layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-openembedded/meta-multimedia" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-openembedded/meta-multimedia layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+else
+	echo "meta-openembedded/meta-multimedia layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-openembedded/meta-networking" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-openembedded/meta-networking layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-networking
+else
+	echo "meta-openembedded/meta-networking layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-raspberrypi layer"
+	bitbake-layers add-layer ../meta-raspberrypi
+else
+	echo "meta-raspberrypi layer already exists"
 fi
 
 set -e
