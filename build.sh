a96a79177e2b03c091429b7e53dd422010f9e60b
Eduard Polyakov <eduardpo@gmail.com>
2025-04-11 20:42:34 +0300

    bring up yocto for qemuarm64 eduardpo/doorbell_rpi_security_camera#1

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ git submodule update
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env
 
-CONFLINE="MACHINE = \"raspberrypi4-64\""
+CONFLINE="MACHINE = \"qemuarm64\""
+#CONFLINE="MACHINE = \"raspberrypi4-64\""
 
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
@@ -22,96 +23,39 @@ else
 	echo "${CONFLINE} already exists in the local.conf file"
 fi
 
+# bitbake-layers show-layers | grep "meta-aesd" > /dev/null
+# layer_info=$?
 
-bitbake-layers show-layers | grep "meta-aesd" > /dev/null
-layer_info=$?
+# if [ $layer_info -ne 0 ];then
+# 	echo "Adding meta-aesd layer"
+# 	bitbake-layers add-layer ../meta-aesd
+# else
+# 	echo "meta-aesd layer already exists"
+# fi
 
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-aesd layer"
-	bitbake-layers add-layer ../meta-aesd
-else
-	echo "meta-aesd layer already exists"
-fi
+declare -a layer_specs=(
+  "meta-aesd|../meta-aesd"
+  "poky/meta|../poky/meta"
+  "poky/meta-poky|../poky/meta-poky"
+  "poky/meta-yocto-bsp|../poky/meta-yocto-bsp"
+  "meta-openembedded/meta-oe|../meta-openembedded/meta-oe"
+  "meta-openembedded/meta-python|../meta-openembedded/meta-python"
+  "meta-openembedded/meta-multimedia|../meta-openembedded/meta-multimedia"
+  "meta-openembedded/meta-networking|../meta-openembedded/meta-networking"
+  "meta-raspberrypi|../meta-raspberrypi"
+)
 
-bitbake-layers show-layers | grep "poky/meta" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding poky/meta layer"
-	bitbake-layers add-layer ../poky/meta
-else
-	echo "poky/meta layer already exists"
-fi
-
-bitbake-layers show-layers | grep "poky/meta-poky" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding poky/meta-poky layer"
-	bitbake-layers add-layer ../poky/meta-poky
-else
-	echo "poky/meta-poky layer already exists"
-fi
-
-bitbake-layers show-layers | grep "poky/meta-yocto-bsp" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding poky/meta-yocto-bsp layer"
-	bitbake-layers add-layer ../poky/meta-yocto-bsp
-else
-	echo "poky/meta-yocto-bsp layer already exists"
-fi
-
-bitbake-layers show-layers | grep "meta-openembedded/meta-oe" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-openembedded/meta-oe layer"
-	bitbake-layers add-layer ../meta-openembedded/meta-oe
-else
-	echo "meta-openembedded/meta-oe layer already exists"
-fi
-
-bitbake-layers show-layers | grep "meta-openembedded/meta-python" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-openembedded/meta-python layer"
-	bitbake-layers add-layer ../meta-openembedded/meta-python
-else
-	echo "meta-openembedded/meta-python layer already exists"
-fi
-
-bitbake-layers show-layers | grep "meta-openembedded/meta-multimedia" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-openembedded/meta-multimedia layer"
-	bitbake-layers add-layer ../meta-openembedded/meta-multimedia
-else
-	echo "meta-openembedded/meta-multimedia layer already exists"
-fi
-
-bitbake-layers show-layers | grep "meta-openembedded/meta-networking" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-openembedded/meta-networking layer"
-	bitbake-layers add-layer ../meta-openembedded/meta-networking
-else
-	echo "meta-openembedded/meta-networking layer already exists"
-fi
-
-bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-raspberrypi layer"
-	bitbake-layers add-layer ../meta-raspberrypi
-else
-	echo "meta-raspberrypi layer already exists"
-fi
+for spec in "${layer_specs[@]}"; do
+  IFS='|' read -r layer_name layer_path <<< "$spec"
+  if ! (bitbake-layers show-layers 2>/dev/null | grep -q "$layer_name"); then
+    echo "Adding $layer_name layer"
+    bitbake-layers add-layer "$layer_path"
+  else
+    echo "$layer_name layer already exists"
+  fi
+done
 
 set -e
-bitbake core-image-aesd
+bitbake core-image-minimal
+#bitbake core-image-base
+#bitbake core-image-aesd
